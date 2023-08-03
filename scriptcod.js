e805def05bfeb76d0c7f5e2fc6ab411b991d64af
Pablo <pablopenia.dev@gmail.com>
2023-08-02 23:10:22 -0300

    refactor filenames and stuff

--- scriptcod.js
+++ scriptcod.js
@@ -1,33 +1,32 @@
-const carrusel = document.querySelector('.carrusel');
-const images = carrusel.querySelectorAll('img');
+const carrousel = document.querySelector('.carrousel')
+const images = carrousel.querySelectorAll('img')
 
-
-let counter = 0;
-const slideWidth = images[0].clientWidth;
-const slideInterval = 9000; // Intervalo de tiempo entre las transiciones (en milisegundos)
+let counter = 0
+const slideWidth = images[0].clientWidth
+const slideInterval = 9000 // Intervalo de tiempo entre las transiciones (en milisegundos)
 
 // Función para pasar a la siguiente imagen
 function nextSlide() {
-  counter++;
-  carrusel.style.transform = `translateX(${-slideWidth * counter}px)`;
+	counter++
+	carrousel.style.transform = `translateX(${-slideWidth * counter}px)`
 
-  // Si alcanza el final del carrusel, vuelve al principio
-  if (counter === images.length) {
-    counter = 0;
-    setTimeout(() => {
-      carrusel.style.transform = 'translateX(0)';
-    }, slideInterval / 2); // Tiempo de espera antes de volver al principio (la mitad del intervalo de tiempo)
-  }
+	// Si alcanza el final del carrousel, vuelve al principio
+	if (counter === images.length) {
+		counter = 0
+		setTimeout(() => {
+			carrousel.style.transform = 'translateX(0)'
+		}, slideInterval / 2) // Tiempo de espera antes de volver al principio (la mitad del intervalo de tiempo)
+	}
 }
 
 // Iniciar el movimiento automático
-let slideTimer = setInterval(nextSlide, slideInterval);
+let slideTimer = setInterval(nextSlide, slideInterval)
 
-// Funciones para pausar/reanudar el movimiento automático al interactuar con el carrusel
-carrusel.addEventListener('mouseenter', () => {
-  clearInterval(slideTimer);
-});
+// Funciones para pausar/reanudar el movimiento automático al interactuar con el carrousel
+carrousel.addEventListener('mouseenter', () => {
+	clearInterval(slideTimer)
+})
 
-carrusel.addEventListener('mouseleave', () => {
-  slideTimer = setInterval(nextSlide, slideInterval);
-});
+carrousel.addEventListener('mouseleave', () => {
+	slideTimer = setInterval(nextSlide, slideInterval)
+})
