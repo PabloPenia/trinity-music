f19aaac75a842bd1cbc0b1a296e3f11376ae7aa0
Pablo Peña <88749586+PabloPenia@users.noreply.github.com>
2023-10-19 10:39:39 -0300

    Refactorize estilos y js

--- scripts/app.js
+++ scripts/app.js
@@ -8,17 +8,32 @@ import {
 
 document.addEventListener('DOMContentLoaded', async () => {
   updateCart() // si habian productos en el carro en una sesion anterior los carga
-  const productsDb = await getProducts()
-  const featured = getFeaturedProducts(productsDb)
-  displayProducts(featured) // muestra los productos en la galeria
-  //* BOTONES
-  const addToCartButtons = document.querySelectorAll('.cart-btn')
-  const showCartBtn = document.getElementById('show-cart-btn')
+  if (window.location) {
+    const isIndex =
+      window.location.pathname === '/index.html' ||
+      window.location.pathname === '/'
 
-  //* LISTENERS
-  addToCartButtons.forEach((button) => {
-    button.addEventListener('click', addToCart)
-  })
+    if (isIndex) {
+      const productsDb = await getProducts()
+      const featured = getFeaturedProducts(productsDb)
+      displayProducts(featured) // muestra los productos en la galeria
+
+      const addToCartButtons = document.querySelectorAll('.cart-btn')
+      addToCartButtons.forEach((button) => {
+        button.addEventListener('click', addToCart)
+      })
+
+      // Modal Suscribirse
+      const cerrarVentanaBtn = document.getElementById('cerrarmiModal')
+      const miModal = document.getElementById('miModal')
+
+      cerrarVentanaBtn.addEventListener('click', function () {
+        return (miModal.style.display = 'none')
+      })
+    }
+  }
+
+  const showCartBtn = document.getElementById('show-cart-btn')
   showCartBtn.addEventListener('click', () =>
     showCartBtn.classList.toggle('active')
   )
@@ -30,35 +45,25 @@ document.addEventListener('DOMContentLoaded', async () => {
   const mostrarNosotrosBtn = document.getElementById('mostrar-nosotros-btn')
   const nosotros = document.getElementById('nosotros')
 
-  mostrarContactoBtn.addEventListener("click", function () {
-    nosotros.classList.remove("active")
-    contacto.classList.toggle("active")
+  mostrarContactoBtn.addEventListener('click', function () {
+    nosotros.classList.remove('active')
+    contacto.classList.toggle('active')
 
-    if (contacto.classList.contains("active")) {
+    if (contacto.classList.contains('active')) {
       // Scroll to the newly displayed element
-      contacto.scrollIntoView({ behavior: "smooth" });
+      contacto.scrollIntoView({ behavior: 'smooth' })
     }
- })
+  })
 
- 
+  mostrarNosotrosBtn.addEventListener('click', function () {
+    contacto.classList.remove('active')
+    nosotros.classList.toggle('active')
 
-  mostrarNosotrosBtn.addEventListener("click", function () {
-    contacto.classList.remove("active")
-    nosotros.classList.toggle("active")
-
-    if (nosotros.classList.contains("active")) {
+    if (nosotros.classList.contains('active')) {
       // Scroll to the newly displayed element
-      nosotros.scrollIntoView({ behavior: "smooth" });
+      nosotros.scrollIntoView({ behavior: 'smooth' })
     }
-})
-
-
+  })
 
   //
-  const cerrarVentanaBtn = document.getElementById('cerrarmiModal')
-  const miModal = document.getElementById('miModal')
-
-  cerrarVentanaBtn.addEventListener('click', function () {
-    return (miModal.style.display = 'none')
-  })
 })
